b4f0dedc5b1aac6e4020a10ee5682d6805f51586
Merlito456 <rabanes.johncarlo1994@gmail.com>
2026-09-02 23:36:40 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/auth/ProtectedRoute';
+import Layout from './components/Layout';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import SiteMasterlist from './components/sites/SiteMasterlist';
@@ -16,28 +17,95 @@ import EngineerPage from './components/engineers/EngineerPage';
 import PersonnelManagement from './components/personnel/PersonnelManagement';
 import ProjectReport from './components/reports/ProjectReport';
 import AboutPage from './components/about/AboutPage';
-import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
     return (
         <AuthProvider>
             <Router>
-                <Navigation />
                 <Routes>
+                    {/* Public routes - No Layout */}
                     <Route path="/login" element={<Login />} />
-                    <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-                    <Route path="/sites" element={<ProtectedRoute><SiteMasterlist /></ProtectedRoute>} />
-                    <Route path="/sites/add" element={<ProtectedRoute><SiteAdd /></ProtectedRoute>} />
-                    <Route path="/sites/:id" element={<ProtectedRoute><SiteDetails /></ProtectedRoute>} />
-                    <Route path="/raawa" element={<ProtectedRoute><RaawaMonitoring /></ProtectedRoute>} />
-                    <Route path="/towerco" element={<ProtectedRoute><TowercoMonitoring /></ProtectedRoute>} />
-                    <Route path="/deployment/plan" element={<ProtectedRoute><PlanDeployment /></ProtectedRoute>} />
-                    <Route path="/deployment/actual" element={<ProtectedRoute><ActualDeployment /></ProtectedRoute>} />
-                    <Route path="/engineer" element={<ProtectedRoute><EngineerPage /></ProtectedRoute>} />
-                    <Route path="/personnel" element={<ProtectedRoute><PersonnelManagement /></ProtectedRoute>} />
-                    <Route path="/reports" element={<ProtectedRoute><ProjectReport /></ProtectedRoute>} />
                     <Route path="/about" element={<AboutPage />} />
+                    
+                    {/* Protected routes with Layout */}
+                    <Route path="/" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <Dashboard />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/sites" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <SiteMasterlist />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/sites/add" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <SiteAdd />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/sites/:id" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <SiteDetails />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/raawa" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <RaawaMonitoring />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/towerco" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <TowercoMonitoring />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/deployment/plan" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <PlanDeployment />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/deployment/actual" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <ActualDeployment />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/engineer" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <EngineerPage />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/personnel" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <PersonnelManagement />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/reports" element={
+                        <ProtectedRoute>
+                            <Layout>
+                                <ProjectReport />
+                            </Layout>
+                        </ProtectedRoute>
+                    } />
                     <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
             </Router>
