baf835595c9a4e4756fa746348958fafcdca10ca
Merlito456 <rabanes.johncarlo1994@gmail.com>
2026-09-03 00:01:29 +0800

    Create NotificationCenter.tsx

---
+++ src/components/dashboard/NotificationCenter.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotificationCenter: React.FC<{ notifications: any[] }> = ({ notifications }) => {
+  return <div>Notifications - Coming Soon</div>;
+};
+
+export default NotificationCenter;
