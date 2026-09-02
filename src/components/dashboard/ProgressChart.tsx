b898fbf5351a269d33929c2d1852cb095c9bade6
Merlito456 <rabanes.johncarlo1994@gmail.com>
2026-09-03 00:01:14 +0800

    Create ProgressChart.tsx

---
+++ src/components/dashboard/ProgressChart.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProgressChart: React.FC<{ projects: any[] }> = ({ projects }) => {
+  return <div>Progress Chart - Coming Soon</div>;
+};
+
+export default ProgressChart;
